5d50754acb2847b77579b0221288642e6290a893
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2026-01-30 16:11:36 +0500

    Add Mongoose schemas and validation for user messages and authentication

---
+++ src/schemas/signupSchema.ts
@@ -0,0 +1,30 @@
+import z from "zod";
+
+const usernameValidation = z
+ .string()
+ .min(2, { message: "Username must be at least 2 characters" })
+ .max(20, { message: "Username must be no more than 20" })
+ .regex(/^[a-zA-Z0-9_]+$/, {
+  message: "Username must not contain special character",
+ });
+
+const emailValidation = z
+ .string()
+ .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+  message: "Invalid email address",
+ });
+
+const passwordValidation = z
+ .string()
+ .min(6, { message: "password must be atleast 6 characters." })
+ .max(20, { message: "" })
+ .regex(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+  { message: "Weak Password" },
+ );
+
+export const signupValidation = z.object({
+ username: usernameValidation,
+ email: emailValidation,
+ password: passwordValidation,
+});
